0c4c0163fe7ea265601da64876af39fea39b782c
Lukas Olsen <lukmarwil@gmail.com>
2024-06-25 23:04:12 +0200

    fix(tsup): fixed tsup library handling to include it

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,3 +1,4 @@
+import { builtinModules } from "node:module";
 import { defineConfig } from "tsup";
 
 const isDev = process.env.npm_lifecycle_event === "dev";
@@ -6,9 +7,17 @@ const isDev = process.env.npm_lifecycle_event === "dev";
 export default defineConfig({
   clean: true,
   entry: ["src/index.ts"],
-  format: ["esm", "cjs"],
-  minify: !isDev,
-  target: "esnext",
+  format: ["esm", "cjs"], // Generate both ESM and CommonJS modules
+  minify: !isDev, // Minify the output in non-dev mode
+  target: "esnext", // Target modern JavaScript
+  dts: true, // Generate TypeScript declaration files
+  sourcemap: "inline", // Include inline source maps
+
+  // Only mark Node.js built-in modules as external
+  external: [...builtinModules],
+  bundle: true,
+  skipNodeModulesBundle: false,
+  noExternal: ["pg", "ora", "chalk", "fs-extra", "inquirer", "yargs", "node-forge"],
 
   outDir: "dist",
   onSuccess: isDev ? "node dist/index.js" : undefined,
